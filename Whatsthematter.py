dbe4d04d00a62ee268117c734532656406a86eaa
Ava Keras <159186728+avakeras@users.noreply.github.com>
2025-01-06 20:23:42 -0500

    Update Whatsthematter.py

--- Whatsthematter.py
+++ Whatsthematter.py
@@ -15,58 +15,51 @@ while True:
         print("This is not an organic molecule :( Try again!")
         continue
 
-    # Counts the number of carbon and hydrogen atoms
-    i = 0
-    sumC = 0
-    sumH = 0
+    # Counts number of carbon and hydrogen molecules
+    sumC = sumH = 0
     for i in range(len(mol)):
         if mol[i] == "C":
-            if mol[i + 1].isnumeric() == True:
-                sumC += int(mol[i + 1])
-            else:
-                sumC += 1
-        if mol[i] == "H" and mol[i] != mol[-1]:
-            if mol[i + 1].isnumeric() == True:
-                sumH += int(mol[i + 1])
-            else:
-                sumH += 1     
-        i += 1    
+            sumC += int(mol[i + 1]) if mol[i + 1].isnumeric() else 1
+        elif mol[i] == "H" and (i + 1 < len(mol)) and mol[i + 1].isnumeric():
+            sumH += int(mol[i + 1])
+        elif mol[i] == "H":
+            sumH += 1
 
-    # Provides a prefix based on the number of carbon molecules
+    # Assigns proper prefix based on number of carbon molecules
     def prefix(sumC):
         prefixes = ["", " methan", "n ethan", " propan", " butan", " pentan", " hexan", " heptan", " octan", " nonan"]
         return prefixes[sumC] if 1 <= sumC <= 9 else ""
-
+        
     def statement(sumC):
         return ("This is a" + prefix(sumC))
-
-    # Identifies functional groups with -OH and -HO endings
-    if mol[-2] == "H" and mol[-1] == "O": 
-       print(statement(sumC) + "al molecule!") #aldehyde
-       break
-    if mol[-2] == "O" and mol[-1] == "H": 
-        if mol[-3] != "O":
-            print(statement(sumC) + "ol molecule!") #alcohol
-            break
+        
+    # Identifies functional groups with -OH ending
+    if mol.endswith("OH"):
+        if mol[-3:] == "O":
+            print(statement(sumC) + "oic acid molecule!")  # carboxylic acid
         else:
-            print(statement(sumC) + "oic acid molecule!") #carboxylic acid
-            break
+            print(statement(sumC) + "ol molecule!")  # alcohol
+        break
 
-    # Identifies functional groups containing "O"
+    # Identifies functional group with -HO ending
+    if mol.endswith("HO"):
+        print(statement(sumC) + "al molecule!")  # aldehyde
+        break
+
+    # Identifies other functional groups containing "O"
     if "O" in mol:
-        if mol[mol.find("O") - 1] == "O" or mol[mol.find("O") + 1] == "O":
-            print("This molecule is an ester!") #ester
-            break
-        elif mol[mol.find("O") - 1] == "C":
-            print(statement(sumC) + "one molecule!") #ketone
-            break
+        o_pos = mol.find("O")
+        if mol[o_pos - 1] == "O" or mol[o_pos + 1] == "O":
+            print("This molecule is an ester!")  # ester
+        elif mol[o_pos - 1] == "C":
+            print(statement(sumC) + "one molecule!")  # ketone
         else:
-            print("This molecule is an ether!") #ether
-            break
+            print("This molecule is an ether!")  # ether
+        break
 
-# If no other functional groups present, assigns the molecule as an alkane, alkene, or alkyne
-# Doesn't work for sums of C and H over 9
-# Doesn't work for molecules with more than one double / triple bond
+    # If no functional groups present, assigns the molecule as an alkane, alkene, or alkyne
+    # Doesn't work for sums of C and H over 9
+    # Doesn't work for molecules with more than one double / triple bond
     if sumC == (sumH/2): #alkene
         print("This is a" + prefix(sumC).replace(prefix(sumC)[-2], "e")+ "e molecule!")
         break
