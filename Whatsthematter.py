08c6b9592d22198a2a8b18208a62e8dea262dfb8
Ava Keras <159186728+avakeras@users.noreply.github.com>
2025-01-15 10:00:43 -0500

    Update Whatsthematter.py

--- Whatsthematter.py
+++ Whatsthematter.py
@@ -2,18 +2,16 @@
 
 # Does not work with amines or amides, halogenoalkanes, nor molecules with more than one functional group (including double and triple bonds).
 # Cannot print specific names for esters or ethers due to potential side chains and more complicated nomenclature.
-# Ensure all letters of the strucutral formula are in uppercase. 
+# Ensure all letters of the strucutral formula are in uppercase, as this is how structural formulae are written.
+# Feel free to try this with condensed formulae too!
 
-while True:
+class WhatsTheMatter:
+    def __init__(self, mol):
+        self.mol = mol
+        self.sumC = 0
+        self.sumH = 0
+        
     mol = input("Enter the molecular formula for an organic compound:")
-
-    if mol == "":
-        print("Where's your molecule? Try again!")
-        continue
-
-    if "C" not in mol:
-        print("This is not an organic molecule :( Try again!")
-        continue
 
     # Counts number of carbon and hydrogen molecules
     sumC = sumH = 0
@@ -25,51 +23,50 @@ while True:
         elif mol[i] == "H":
             sumH += 1
 
-    # Assigns proper prefix based on number of carbon molecules
+    # Assigns prefix based on carbon chain length
     def prefix(sumC):
         prefixes = ["", " methan", "n ethan", " propan", " butan", " pentan", " hexan", " heptan", " octan", " nonan"]
         return prefixes[sumC] if 1 <= sumC <= 9 else ""
-        
-    def statement(sumC):
+
+    # Makes a statement so I don't need to keep writing it
+    def statement(prefix, sumC):
         return ("This is a" + prefix(sumC))
+
+    # Troubleshooting stuff
+    if mol == "" or "C" not in mol:
+        print("Where's your molecule? Try again!")
         
     # Identifies functional groups with -OH ending
-    if mol.endswith("OH"):
-        if mol[-3:] == "O":
-            print(statement(sumC) + "oic acid molecule!")  # carboxylic acid
+    elif mol.endswith("OH"):
+        if mol[-3] == "O":
+            print(statement(prefix, sumC) + "oic acid molecule!")  # Carboxylic acids
         else:
-            print(statement(sumC) + "ol molecule!")  # alcohol
-        break
+            print(statement(prefix, sumC) + "ol molecule!")  # Alcohols
 
     # Identifies functional group with -HO ending
-    if mol.endswith("HO"):
-        print(statement(sumC) + "al molecule!")  # aldehyde
-        break
+    elif mol.endswith("HO"):
+        print(statement(prefix, sumC) + "al molecule!")  # Aldehydes
 
     # Identifies other functional groups containing "O"
-    if "O" in mol:
+    elif "O" in mol:
         o_pos = mol.find("O")
         if mol[o_pos - 1] == "O" or mol[o_pos + 1] == "O":
-            print("This molecule is an ester!")  # ester
+            print("This molecule is an ester!")  # Esters
         elif mol[o_pos - 1] == "C":
-            print(statement(sumC) + "one molecule!")  # ketone
+            print(statement(prefix, sumC) + "one molecule!")  # Ketones
         else:
-            print("This molecule is an ether!")  # ether
-        break
+            print("This molecule is an ether!")  # Ethers
 
     # If no functional groups present, assigns the molecule as an alkane, alkene, or alkyne
     # Doesn't work for sums of C and H over 9
     # Doesn't work for molecules with more than one double / triple bond
-    if sumC == (sumH/2): #alkene
+    elif sumC == (sumH/2): # Alkenes
         print("This is a" + prefix(sumC).replace(prefix(sumC)[-2], "e")+ "e molecule!")
-        break
-    if sumC == ((sumH+2) / 2): #alkyne
+    elif sumC == ((sumH+2) / 2): # Alkynes
         print("This is a" + prefix(sumC).replace(prefix(sumC)[-2], "y")+ "e molecule!")
-        break
-    if sumC == ((sumH-2) / 2): #alkane
-        print("This is a" + prefix(sumC) + "e molecule!")
-        break
-    if sumC == 6 and sumH == 6: #benzene
+    elif sumC == ((sumH-2) / 2): # Alkanes
+        print("This is a" + prefix(sumC) + "e molecule!")    
+    elif sumC == 6 and sumH == 6: # Benzene
         print("This is a benzene molecule!")
-        break
-
+    else: 
+        print("Either you did not type an organic molecule, or the one you typed is not compatible with WhatsTheMatter. Try again!")
